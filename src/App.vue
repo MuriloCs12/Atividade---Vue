0a9a14bdab1da7512e9eb4210d9564c871b920d1
MuriloCs12 <133374714+MuriloCs12@users.noreply.github.com>
2025-10-10 11:13:03 +0000

    terminando atividade

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
 <form @submit.prevent="adicionarMensagem">
   <input v-model.trim="novaMensagem" placeholder="Digite a mensagem" />
-  <button>Adicionar</button>
+  <button :disabled="!novaMensagem">Adicionar</button>
 </form>
+<p v-if="erro" class='erro'>{{erro}}</p>
+<input type="text" v-model="filtro" placeholder="Buscar mensagem">
+
+<p v-if='filtro'>Exibindo {{ buscar.length }} de {{estado.itens.length}}</p>
 
 <ul v-if="estado.itens.length">
-  <li v-for="m in estado.itens" :key="m.id">
+  <button class='limpar' @click="limpar">Limpar</button>
+  <li :class="{ longo: m.texto.length > 30 }" v-for="m in buscar" :key="m.id">
     #{{ m.id }} — {{ m.texto }}
+    <button @click = "removerMensagem(m.id)">Delete</button>
   </li>
 </ul>
 <p v-else>Lista vazia.</p>
 </template>
 
 <script setup>
-import { reactive, ref } from 'vue'
+import { reactive, computed, ref } from 'vue'
 
 const estado = reactive({
   nextId: 3,
@@ -24,19 +30,34 @@ const estado = reactive({
 })
 
 const novaMensagem = ref('')
+const erro = ref('')
 
 function adicionarMensagem() {
-  if (!novaMensagem.value) return
+  if (!novaMensagem.value) {
+    erro.value = 'O campo não pode estar vazio.'
+    return
+  }
   estado.itens.push({
     id: estado.nextId++,
     texto: novaMensagem.value
   })
   novaMensagem.value = ''
+  erro.value = ''
 }
 
 function removerMensagem(id) {
   estado.itens = estado.itens.filter(m => m.id !== id)
 }
+
+function limpar() {
+  estado.itens = []
+}
+
+const filtro = ref('')
+
+const buscar = computed(() => estado.itens.filter((m) => m.texto.toLowerCase().includes(filtro.value.toLowerCase())))
+
+
 </script>
 
 <style scoped>
@@ -45,4 +66,6 @@ input { padding: 6px; margin-right: 6px; }
 ul { margin-top: 10px; padding-left: 20px; }
 li { margin-bottom: 6px; }
 button { cursor: pointer; }
+.limpar {margin-bottom: 10px}
+.longo {font-weight: 800}
 </style>
